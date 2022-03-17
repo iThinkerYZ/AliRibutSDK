556e332448e8e7c8812a2206363af6dd83e5aa58
weixiao.yz <weixiao.yz@alibaba-inc.com>
2022-03-17 11:30:32 +0800

    update

---
+++ AliRibutSDK/AliRibutSDK.h
@@ -0,0 +1,21 @@
+//
+//  AliRibutSDK.h
+//  AliRibutSDK
+//
+//  Created by 微笑 on 2022/3/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AliRibutSDK.
+FOUNDATION_EXPORT double AliRibutSDKVersionNumber;
+
+//! Project version string for AliRibutSDK.
+FOUNDATION_EXPORT const unsigned char AliRibutSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AliRibutSDK/PublicHeader.h>
+
+#import <AliRibutSDK/AliRibutProtocol.h>
+#import <AliRibutSDK/AliRibutManager.h>
+
+
